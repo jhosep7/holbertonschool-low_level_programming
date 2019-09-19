1fe84462b370d632b91c3d3766466aa0cc74424d
Jose Diaz <jodia.ms@outlook.com>
2019-09-18 22:43:40 -0400

    101-quote.c2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
-*hello
+*main - Entry point
+*
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
