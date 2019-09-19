bd6f747a718be855eb433f496c84c43ad3e6d1eb
Jose Diaz <jodia.ms@outlook.com>
2019-09-19 14:27:27 -0400

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ n = rand() - RAND_MAX / 2;
 lastD = n % 10;
 if (lastD > 5)
 {
-printf("Last digit of %d is %d and is greater than 5", n, lastD);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastD);
 }
 else if (lastD == 0)
 {
-printf("Last digit of %d is %d and is 0", n, lastD);
+printf("Last digit of %d is %d and is 0\n", n, lastD);
 }
 else if (lastD > 0 && lastD < 6)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0", n, lastD);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastD);
 }
 return (0);
 }
