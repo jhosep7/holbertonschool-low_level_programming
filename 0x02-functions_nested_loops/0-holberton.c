1fa1d3cf9b397c629a4fd426d2633549632a0730
Jose Diaz <jodia.ms@outlook.com>
2019-09-24 09:31:47 -0400

    0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -3,11 +3,10 @@
 int main(void)
 {
     int i;
-    char a[] = { 'H', 'o', 'l', 'b', 'e','r','t','o','n', '\0' };
-    
-    for (i=0;i<=9;i++)
+    char a[] = { 'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\0' };
+    for (i = 0; i <= 9; i++)
     {
-    _putchar(a[i]);	
+    _putchar(a[i]);
     }
     _putchar('\n');
     return (0);
