d4a9ac53f4a94b3883b0e03fe1b9d54fef865230
Jose Diaz <jodia.ms@outlook.com>
2019-09-24 10:03:18 -0400

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,9 +6,9 @@
  */
 void print_alphabet(void)
 {
-  for (int i = 'a'; i <= 'z'; i++)
-  {
-    _putchar(i);
-  }
-  _putchar('\n');
+for (int i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
