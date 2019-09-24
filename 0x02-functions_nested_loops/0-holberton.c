fd2d27d09d59516a0bc20e11152747d16f7a1e70
Jose Diaz <jodia.ms@outlook.com>
2019-09-24 09:34:41 -0400

    0-holberton.c2

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,14 +1,17 @@
 #include"holberton.h"
-
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    int i;
-    char a[] = { 'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\0' };
-    for (i = 0; i <= 9; i++)
-    {
-    _putchar(a[i]);
-    }
-    _putchar('\n');
-    return (0);
+int i;
+char a[] = { 'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\0' };
+for (i = 0; i <= 9; i++)
+{
+_putchar(a[i]);
 }
-
+_putchar('\n');
+return (0);
+}
