301ff92a056193b0e11cc2b8cc1ec9770b8b6ed9
Jose Diaz <jodia.ms@outlook.com>
2019-09-24 23:16:49 -0400

    1-main.cFixed2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - returns the largest of 3 numbers
- * @void: void
- * description: Comment out the part causing the loop
- * Return: 0
- */
+* main - causes an infinite loop
+* description: the part causing the loop was commented
+* Return: 0
+*/
 
 int main(void)
 {
 int i;
 printf("Infinite loop incoming :(\n");
+i = 0;
 /*
- * i = 0;
  * while (i < 10)
  * {
  *	putchar(i);
