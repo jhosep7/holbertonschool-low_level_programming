e5874290b72cc6e598390b8f46a578d47f7a992e
Jose Diaz <jodia.ms@outlook.com>
2019-09-24 21:55:30 -0400

    0x03-debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * main - tests function that prints if integer is Post or Neg
+ * Return: 0
+ */
+int main(void)
+{
+int Test;
+Test = 0;
+positive_or_negative(i);
+return (0);
+}
