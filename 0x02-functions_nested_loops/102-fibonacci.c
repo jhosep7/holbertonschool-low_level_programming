895fab8e3b9b2baaa82b17d86d81b771350cec48
Jose Diaz <jodia.ms@outlook.com>
2019-09-24 21:03:35 -0400

    102-fibonacci.c3

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,22 +1,47 @@
-#include "stdio.h"
+#include <stdio.h>
 /**
- * fibonacci - prints the first 50 Fibonacci numbers
+ * main - prints the first 98 number
  *
- * description: starting with 1 and 2, followed by a new line.
- *
- * Return: int
+ * Description: fibobnachi numbers starting from one
+ * Return: 0
  */
 int main(void)
 {
-int  x = 1;
-long  x1 = 0, x2 = 1, y;
-while (int i <= 50)
+long fib1, fib1a, fib1b, fib2, fib2a, fib2b, fib3, fib3a, fib3b;
+long divi = 10000000000;
+fib1 = 0;
+fib2 = 1;
+int i = 1;
+for (i = 0; i < 91; i++)
 {
-y = x1 + x2;
-printf("%ld, ", y);
-x1 = x2;
-x2 = y;
-i++;
+fib3 =  fib2 + fib1;
+fib1 = fib2;
+fib2 = fib3;
+printf("%ld, ", fib3);
+}
+fib1a = fib1 / divi;
+fib1b = fib1 % divi;
+fib2a = fib2 / divi;
+fib2b = fib2 % divi;
+for (; i < 98; i++)
+{
+fib3a = fib1a + fib2a;
+fib3b = fib1b + fib2b;
+if (fib3b > divi)
+{
+fib3b = fib3b % divi;
+fib3a++;
+}
+if (i != 97)
+{
+printf("%ld%ld, ", fib3a, fib3b);
+fib1a = fib2a;
+fib1b = fib2b;
+fib2a = fib3a;
+fib2b = fib3b;
+}
+else
+printf("%ld%ld\n", fib3a, fib3b);
 }
 return (0);
 }
