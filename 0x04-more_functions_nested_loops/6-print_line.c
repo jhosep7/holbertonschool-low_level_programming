ffda67b41ee3242a7aeebdba5103b916a55a34f1
Jose Diaz <jodia.ms@outlook.com>
2019-09-26 15:45:58 -0400

    6-print_line.c2

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,18 @@
 #include "holberton.h"
-/**
- * print_line - function that draws a straight line in the terminl
- * @n: int type
- * Return: none
- */
 
+/**
+ * print_line - draw a straight line
+ * @n: number of times line is printed
+ * Description: Can only use _putchar to print
+ */
 void print_line(int n)
 {
-for (int i = 0; i < n; i++)
+int i;
+i = 0;
+while (i < n)
 {
 _putchar('_');
+i++;
 }
-_putchar ('\n');
+_putchar('\n');
 }
