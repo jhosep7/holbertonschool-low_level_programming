9ad5715fbe9862e44d1619191fbf476da1852891
Jose Diaz <jodia.ms@outlook.com>
2019-10-03 23:06:51 -0400

    6-cap_string.c2

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,28 +8,27 @@
 
 char *cap_string(char *Arr)
 {
-  char SpChar[13] ={'\t', '\n', '(', ')', '{', '}', ' ', ',', ';', '.', '!', '?',};
-  int i, j;
-
-  for (i = 0; Arr[i]; i++)
-    {
-      for (j = 0; SpChar[j]; j++)
-	{
-	  if (Arr[i] == SpChar[j])
-	    {
-	      if (Arr[i + 1] >= 'a' && Arr[i + 1] <= 'z')
-		{
-		  Arr[i + 1] = Arr[i + 1] - 32;
-		}
-	    }
-	  if (i == 0)
-	    {
-	      if (Arr[i] >= 'a' && Arr[i] <='z')
-		{
-		  Arr[i] = Arr[i] - 32;
-		}
-	    }
-	}
-    }
-  return (Arr);
+char SpChar[13] = {'\t', '\n', '(', ')', '{', '}', ' ', ',', ';', '.', '!', '?',};
+int i, j;
+for (i = 0; Arr[i]; i++)
+{
+for (j = 0; SpChar[j]; j++)
+{
+if (Arr[i] == SpChar[j])
+{
+if (Arr[i + 1] >= 'a' && Arr[i + 1] <= 'z')
+{
+Arr[i + 1] = Arr[i + 1] - 32;
+}
+}
+if (i == 0)
+{
+if (Arr[i] >= 'a' && Arr[i] <= 'z')
+{
+Arr[i] = Arr[i] - 32;
+}
+}
+}
+}
+return (Arr);
 }
