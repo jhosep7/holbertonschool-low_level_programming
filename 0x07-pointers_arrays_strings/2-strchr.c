5f83e7650cd46309c3eac03fca4977033b724c6c
Jose Diaz <jodia.ms@outlook.com>
2019-10-07 15:42:29 -0400

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strchr - check the code for Holberton School students.
+ * @s: char
+ * @c: char
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; s[i]; i++)
+{
+if (s[i] == c)
+  {
+  s += i;
+  return (s);
+  }
+}
+ if (c == '\0') 
+s=0;
+ return ('\0');
+}
