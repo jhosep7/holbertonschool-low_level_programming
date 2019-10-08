3843a5bf546b110b2bf005338c8850461fe10ec2
jhosep7 <jodia.ms@outlook.com>
2019-10-07 21:49:54 -0500

    7-print_chessboard2

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - chessboard.
+ * @a: char
+ * Return: Always 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
+}
