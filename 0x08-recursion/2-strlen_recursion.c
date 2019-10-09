90a0ff7cd2ce01e4220fde8d520a1ba25be26b3d
jhosep7 <jodia.ms@outlook.com>
2019-10-09 13:41:01 -0500

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,14 +8,14 @@
  */
 int _strlen_recursion(char *s)
 {
-  int contador;
-  if (*s == '\0')
-    {
-      return (0);
-    }
-  else
-    {
-      contador = 1 + _strlen_recursion(s + 1);
-      return (contador);
-    }
+int contador;
+if (*s == '\0')
+{
+return (0);
+}
+else
+{
+contador = 1 + _strlen_recursion(s + 1);
+return (contador);
+}
 }
