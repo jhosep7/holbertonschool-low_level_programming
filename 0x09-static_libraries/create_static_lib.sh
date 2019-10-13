d85c479214e13dfe687715913985634c0d01c24c
Usuario <jhosep7>
2019-10-12 23:26:54 -0500

    fixed

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/bash                                                                     
+gcc -Wall -pedantic -Werror -Wextra -c *.c
 ar -rc liball.a *.o
 ar -t liball.a
 ranlib liball.a
+
