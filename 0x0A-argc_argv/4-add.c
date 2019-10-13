b0cc7b02e2a02891c7be45396578d4d00f0e64d1
jhosep7 <jhosep7>
2019-10-13 17:48:22 -0500

    4-add.cfixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,15 +14,12 @@ int main(int argc, char *argv[])
 int i, sum;
 for (i = 1; i < argc; i++)
 {
-if (atoi(argv[i]))
-{
-sum = sum + atoi(argv[i]);
-}
-else
+if (!atoi(argv[i]))
 {
 printf("Error\n");
 return (1);
 }
+sum = sum + atoi(argv[i]);
 }
 printf("%i\n", sum);
 return (0);
