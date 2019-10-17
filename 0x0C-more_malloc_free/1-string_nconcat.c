87e7dcab69b1b2f583f278c3572ec4ca2ee69380
jhosep7 <jodia.ms@outlook.com>
2019-10-17 14:36:10 -0500

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,44 +5,41 @@
  * string_nconcat - concatenates two strings..
  * @s1:char
  * @s2:char
- * @n: Uint 
+ * @n: Uint
  * Return: Always 0.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  int i, j, len1, len2;
-  int N = n;
-  char *Arr;
-
-  if (s1 == NULL)
-    {s1 = "" ; }
-  if (s2 == NULL)
-    {s2 = ""; }
-
-  for (i = 0; s1[i] != '\0'; i++)
-    {; }
-  for (j = 0; s2[j] != '\0'; j++)
-    {; }
-
-  if (N >= j)
-    {
-      N = j;
-      Arr = malloc(sizeof(char) * (i + j +1));
-    }
-  else
-    {
-      Arr = malloc(sizeof(char) * (i + N + 1));
-    }
-   if (Arr == NULL)
-	{return (NULL); }
-  for (len1 = 0; len1 < i; len1++)
-    {
-      Arr[len1] = s1[len1];
-    }
-  for (len2 = 0; len2 < N; len2++)
-    {
-      Arr[len1++] = s2[len2];
-    }
-  Arr[len1++]='\0';
-  return (Arr);  
+int i, j, len1, len2;
+int N = n;
+char *Arr;
+if (s1 == NULL)
+{s1 = "" ; }
+if (s2 == NULL)
+{s2 = ""; }
+for (i = 0; s1[i] != '\0'; i++)
+{; }
+for (j = 0; s2[j] != '\0'; j++)
+{; }
+if (j <= N)
+{
+N = j;
+Arr = malloc(sizeof(char) * (i + j + 1));
+}
+else
+{
+Arr = malloc(sizeof(char) * (i + N + 1));
+}
+if (Arr == NULL)
+{return (NULL); }
+for (len1 = 0; len1 < i; len1++)
+{
+Arr[len1] = s1[len1];
+}
+for (len2 = 0; len2 < N; len2++)
+{
+Arr[len1++] = s2[len2];
+}
+Arr[len1++] = '\0';
+return (Arr);
 }
