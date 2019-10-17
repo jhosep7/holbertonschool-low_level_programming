bd43c258163e072187c16e491b0c1a79f8d08597
jhosep7 <jodia.ms@outlook.com>
2019-10-17 16:35:00 -0500

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,22 +9,21 @@
  */
 int *array_range(int min, int max)
 {
-  int *Arr, i;
-
-  if (min > max)
-    {
-      return (NULL);
-    }
-  Arr = malloc (sizeof(int) * (max - min + 1));
-  if (Arr == NULL)
-    {
-      return (NULL);
-    }
-  for (i = 0; min <= max; i++)
-    {
-      Arr[i] = min;
-      min++;
-    }
-  Arr[i] = '\0';
-  return (Arr);
+int *Arr, i;
+int SumLen;
+if (min > max)
+{
+return (NULL);
+}
+SumLen = max - min + 1;
+Arr = malloc(sizeof(int) * (SumLen));
+if (Arr == NULL)
+{
+return (NULL);
+}
+for (i = 0; min <= SumLen; i++)
+{
+Arr[i] = min++;
+}
+return (Arr);
 }
