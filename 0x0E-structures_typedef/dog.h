ef32f3edf30c1f5102be24e08123a832b5a8810c
jhosep7 <jodia.ms@outlook.com>
2019-10-21 15:29:27 -0500

    0x0E-structures_typedef/

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - a struct
+ *@name:name
+ *@age:age
+ *@owner:owner
+ *Description:Little Dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
