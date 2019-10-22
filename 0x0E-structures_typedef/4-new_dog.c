fd9bd9a57b3dc7375aedbd5dd0adde6bc76eee70
jhosep7 <jodia.ms@outlook.com>
2019-10-21 22:05:53 -0500

    4-new_dog.cfixed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,64 +1,46 @@
-#include "dog.h"
 #include <stdlib.h>
-
+#include "dog.h"
 /**
- *LenArray - Lenght of any array comming in
- *@origin: Array comming
- *Return: int
- */
-int LenArray(char *origin)
-{
-int i = 0;
-while (origin[i] != '\0')
-{i++; }
-return (i);
-}
-
-/**
- *CopyArr - Pass all the info to the new Arr
- *@NewArr:char
- *@OldArr:char
- *Return:char
- */
-char *CopyArr(char *NewArr, char *OldArr)
-{
-int j;
-for (j = 0; OldArr[j] != '\0'; j++)
-{NewArr[j] = OldArr[j];	}
-NewArr[j] = '\0';
-return (NewArr);
-}
-
-/**
- * new_dog - creates a new dog.
- * @name:char
- * @age:float
- * @owner:char
- * Return: Always 0.
+ *new_dog - new dog
+ *@name:char
+ *@age:char
+ *@owner:char
+ *
+ *Return:dog_t
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-if (name == NULL || owner == NULL || age < 0)
-{return (NULL);	}
-dog_t *NewDjango;
-NewDjango = malloc(sizeof(dog_t));
-if (NewDjango == NULL)
-{return (NULL); }
-NewDjango->name = malloc(sizeof(char) * (LenArray(name) + 1));
-if (NewDjango->name == NULL)
-{
-free(NewDjango);
-return (NULL);
-}
-NewDjango->owner = malloc(sizeof(char) * (LenArray(owner) + 1));
-if (NewDjango->owner == NULL)
-{
-free(NewDjango);
-free(NewDjango->name);
-return (NULL);
-}
-NewDjango->name = CopyArr(NewDjango->name, name);
-NewDjango->age = age;
-NewDjango->owner = CopyArr(NewDjango->owner, owner);
-return (NewDjango);
+	dog_t *NewDog;
+	int i, j;
+
+	if (name == NULL || owner == NULL)
+	{return (NULL); }
+	NewDog = malloc(sizeof(dog_t));
+	if (NewDog == NULL)
+	{return (NULL); }
+	for (i = 0; name[i] != '\0'; i++)
+	{; }
+	for (j = 0; name[j] != '\0'; j++)
+	{; }
+	NewDog->name = malloc(sizeof(char) * (i + 1));
+	if (NewDog->name == NULL)
+	{
+		free(NewDog);
+		return (NULL);
+	}
+	NewDog->age = age;
+	NewDog->owner = malloc(sizeof(char) * (j + 1));
+	if (NewDog->owner == NULL)
+	{
+		free(NewDog->name);
+		free(NewDog);
+		return (NULL);
+	}
+	int k, l;
+
+	for (k = 0; k <= i; k++)
+	{NewDog->name[k] = name[k]; }
+	for (l = 0; l <= j; l++)
+	{NewDog->owner[l] = owner[l]; }
+	return (NewDog);
 }
