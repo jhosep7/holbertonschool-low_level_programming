2fcd7c5f1788a74b1cdcc331005fd8b13fbc4f6a
jhosep7 <jodia.ms@outlook.com>
2019-10-21 19:57:44 -0500

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,10 +8,10 @@
  */
 int LenArray(char *origin)
 {
-	int i = 0;
-	while (origin[i] != '\0')
-	{i++; }
-	return (i);
+int i = 0;
+while (origin[i] != '\0')
+{i++; }
+return (i);
 }
 
 /**
@@ -22,11 +22,11 @@ int LenArray(char *origin)
  */
 char *CopyArr(char *NewArr, char *OldArr)
 {
-	int j;
-	for (j = 0; OldArr[j] != '\0'; j++)
-	{NewArr[j] = OldArr[j];	}
-	NewArr[j] = '\0';
-	return (NewArr);
+int j;
+for (j = 0; OldArr[j] != '\0'; j++)
+{NewArr[j] = OldArr[j];	}
+NewArr[j] = '\0';
+return (NewArr);
 }
 
 /**
@@ -38,30 +38,27 @@ char *CopyArr(char *NewArr, char *OldArr)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (name == NULL ||owner == NULL || age < 0)
-	{return (NULL);	}
-	
-	dog_t *NewDjango;
-	
-	NewDjango = malloc(sizeof(dog_t));
-	if (NewDjango == NULL)
-	{return (NULL); }
-
-	NewDjango->name = malloc(sizeof(char) * (LenArray(name) + 1));
-	if (NewDjango->name == NULL)
-	{
-		free(NewDjango);
-		return (NULL);
-	}
-        NewDjango->owner = malloc(sizeof(char) * (LenArray(owner) + 1));
-	if (NewDjango->owner == NULL)
-	{
-		free(NewDjango);
-		free(NewDjango->name);
-		return (NULL);
-	}
-   NewDjango->name = CopyArr(NewDjango->name, name);
-   NewDjango->age = age;
-   NewDjango->owner = CopyArr(NewDjango->owner, owner);
-   return (NewDjango);
+if (name == NULL || owner == NULL || age < 0)
+{return (NULL);	}
+dog_t *NewDjango;
+NewDjango = malloc(sizeof(dog_t));
+if (NewDjango == NULL)
+{return (NULL); }
+NewDjango->name = malloc(sizeof(char) * (LenArray(name) + 1));
+if (NewDjango->name == NULL)
+{
+free(NewDjango);
+return (NULL);
+}
+NewDjango->owner = malloc(sizeof(char) * (LenArray(owner) + 1));
+if (NewDjango->owner == NULL)
+{
+free(NewDjango);
+free(NewDjango->name);
+return (NULL);
+}
+NewDjango->name = CopyArr(NewDjango->name, name);
+NewDjango->age = age;
+NewDjango->owner = CopyArr(NewDjango->owner, owner);
+return (NewDjango);
 }
