5c78d1ddfcbb51bc8ce1f4709538b25e46bd45e3
jhosep7 <jodia.ms@outlook.com>
2019-10-21 19:47:34 -0500

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,8 +2,8 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * main - check the code for Holberton School students.
- *
+ * free_dog - free the dog
+ * @d:dog_t
  * Return: Always 0.
  */
 void free_dog(dog_t *d)
