59732d367b290af7dbff00137c9fc4a5ceab3d38
jhosep7 <jodia.ms@outlook.com>
2019-10-23 14:12:40 -0500

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,12 +16,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
+	if (array != NULL && cmp != NULL)
+	{
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 		{
 			return (i);
 		}
+	}
 	}
 	return (-1);
 }
