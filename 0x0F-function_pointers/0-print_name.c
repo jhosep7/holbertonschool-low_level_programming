2355f6fc7f86ceb346434b6788778508effa1adc
jhosep7 <jodia.ms@outlook.com>
2019-10-23 13:27:17 -0500

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "function_pointers.h"
-
 /**
  * print_name - prints a name
  * @name: name of the person
@@ -9,7 +9,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || F == NULL)
-	{return NULL; }
+	if (name == NULL || f == NULL)
+	{return (0); }
 	f(name);
 }
