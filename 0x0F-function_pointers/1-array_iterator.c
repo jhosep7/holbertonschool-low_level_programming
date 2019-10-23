9343c1e0edd5eb713baaa118ca10d1f70372f856
jhosep7 <jodia.ms@outlook.com>
2019-10-23 14:04:24 -0500

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (array != NULL || action != NULL)
 	{
-	for (unsigned int i = 0; i < size; i++)
+		while (size-- > 0)
 	{
-		action(array[i]);
+		action(*array);
+		array++;
 	}
 	}
 }
