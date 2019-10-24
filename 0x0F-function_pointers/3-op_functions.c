66ce47679d1ba5bdfab62c288f7060d14ca8684b
jhosep7 <jodia.ms@outlook.com>
2019-10-23 21:30:29 -0500

    betty passed

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,31 +1,51 @@
 #include "3-calc.h"
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
+/**
+ *op_add- addition
+ *@a:int
+ *@b:int
+ *Return: int
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
+/**
+ *op_sub - substract
+ *@a:int
+ *@b:int
+ *Return: int
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
+/**
+ *op_mul - multiply
+ *@a:int
+ *@b:int
+ *Return: int
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
+/**
+ *op_div - division
+ *@a:int
+ *@b:int
+ *Return: int
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-
+/**
+ *op_mod - modulo
+ *@a:int
+ *@b:int
+ *Return: int
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
