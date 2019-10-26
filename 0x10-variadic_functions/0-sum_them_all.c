1ec7061361bc606a25e651dff18a6e873bafbeb1
jhosep7 <jodia.ms@outlook.com>
2019-10-26 17:28:25 -0500

    0-sum_them_all.c3

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,10 +10,6 @@ int sum_them_all(const unsigned int n, ...)
 	va_list arguments;
 	unsigned int sum = 0;
 
-	if (n == 0)
-	{
-		return (0);
-	}
 	va_start(arguments, n);
 	for (unsigned int x = 0; x < n; x++)
 	{
