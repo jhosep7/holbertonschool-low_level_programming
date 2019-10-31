a78c9d88e6cf8e27ad1cde3f47789244910f5a5b
jhosep7 <jodia.ms@outlook.com>
2019-10-31 18:03:50 -0500

    0-print_list.c2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_list - check the code for Holberton School students.
+ * @h:str
+ * Return: size_t.
  */
 size_t print_list(const list_t *h)
 {
