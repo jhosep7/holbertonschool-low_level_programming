f4b4b2ae242f4059b3d6d2b7776c8ed0dec5f750
jhosep7 <jodia.ms@outlook.com>
2019-10-31 18:28:41 -0500

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+/**
+ * add_node - check the code for Holberton School students.
+ * @head:pointerx2
+ * @str: string
+ * Return: list:t.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int len = 0;
+	char *MyArr;
+	list_t *NewNode;
+
+	NewNode = malloc(sizeof(list_t));
+	if (NewNode == NULL)
+	{return (NULL); }
+	MyArr = strMyArr(str);
+	if (MyArr == NULL)
+	{free(NewNode), return (NULL); }
+	while (str)
+	{len++; }
+	(*NewNode).str = MyArr;
+	(*NewNode).len = len;
+	(*NewNode).nex = *head;
+	*head = NewNode;
+	return (NewNode);
+}
