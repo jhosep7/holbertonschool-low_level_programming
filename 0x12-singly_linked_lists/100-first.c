92ef891953d4e7322f7a4b80a2f30f15ced784dc
jhosep7 <jodia.ms@outlook.com>
2019-10-31 20:30:17 -0500

    100-first.c2

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,9 @@
  *Advanced - Prints before main
  *Returns: void
  */
-void Advanced (void) __attribute__ ((constructor));
-
-void Advanced (void)
+void Advanced(void) __attribute__ ((constructor));
+void Advanced(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
-	);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
