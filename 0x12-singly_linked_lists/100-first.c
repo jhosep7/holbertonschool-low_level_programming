b64d0fa67b37bdba11476466c37e22fa2931ef36
jhosep7 <jodia.ms@outlook.com>
2019-10-31 20:26:49 -0500

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ *Advanced - Prints before main
+ *Returns: void
+ */
+void Advanced (void) __attribute__ ((constructor));
+
+void Advanced (void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+	);
+}
