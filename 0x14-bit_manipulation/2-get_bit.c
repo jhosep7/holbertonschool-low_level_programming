3524cd69abc7373bed05ecf96d146c15a2c09883
jhosep7 <jodia.ms@outlook.com>
2019-11-07 18:56:02 -0500

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * main - check the code for Holberton School students.
- *
+ * get_bit - returns the value of a bit at a given index.
+ * @n: ULI
+ * @index:starting from 0
  * Return: Always 0.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > (sizeof(unsigned long int ) * 8))
-	{return (-1) ;}
+	if (index > (sizeof(unsigned long int) * 8))
+	{return (-1); }
 	n = n >> index;
 	return (n & 1);
 }
