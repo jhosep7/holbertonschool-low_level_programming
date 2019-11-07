d66f8c9e9af447766f0e94a12ac1404fec5283b8
jhosep7 <jodia.ms@outlook.com>
2019-11-07 11:10:50 -0500

    README and more stuff

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	Aux = *head;
 	while (Aux)
 	{
-		if (i == idx - 1)
+		if (i == (idx - 1))
 		{
 			(*NewNd).next = (*Aux).next;
 			(*Aux).next = NewNd;
