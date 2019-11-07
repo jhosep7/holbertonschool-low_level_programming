d610f0b6198048a06d73830edb9c356239a6cc90
jhosep7 <jodia.ms@outlook.com>
2019-11-07 18:20:03 -0500

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,4 @@
 #include "holberton.h"
-unsigned int binary_to_uint(const char *b);
-int _pow_recursion(int x, int y);
-/**
- * _pow_recursion - check the code for Holberton School students.
- * @x: int
- * @y: int
- * Return: int.
- */
-int _pow_recursion(int x, int y)
-{
-	int r;
-
-	if (y < 0)
-	{return (-1); }
-	if (y > 0)
-	{
-		r = x * _pow_recursion(x, (y - 1));
-		return (r);
-	}
-	return (1);
-}
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b:char
@@ -27,8 +6,8 @@ int _pow_recursion(int x, int y)
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int  result = 0, BinaryNum;
-int i, j=0;
+unsigned int  result = 0;
+int i, j = 0;
 
 if (b == NULL)
 {
@@ -42,12 +21,11 @@ for (i = 0; b[i] != '\0'; i++)
 	{
 		return (0);
 	}
+	result = result << 1;
 	if (b[i] == '1')
-	{BinaryNum = 1; }
-	if (b[i] == '0')
-	{BinaryNum = 0; }
-	result += (BinaryNum *_pow_recursion(2,j))/2;
-	j--;
+	{
+		result ^= 1;
+	}
 }
 return (result);
 }
