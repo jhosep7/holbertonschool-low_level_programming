0dfd74bb119c4a87ef5f95cd62d4b0e5bc48f854
jhosep7 <jodia.ms@outlook.com>
2019-11-12 14:42:46 -0500

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * create_file - function that creates a file.
+ * @filename: char const.
+ * @text_content: char
+ * Return: Always 0.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int   Input, Output, i;
+
+	if (filename == NULL)
+	{return (0); }
+	Input = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	if (Input == -1)
+	{return (0); }
+	if (text_content == NULL)
+	{return (1); }
+	for (i = 0; text_content[i] != '\0'; i++)
+	{; }
+	Output =  write(Input, text_content, i);
+	if (Output == -1 || Output != i)
+	{return (-1); }
+	close(Input);
+	return(1);
+}
