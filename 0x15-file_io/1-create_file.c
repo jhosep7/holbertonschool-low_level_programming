1cbd4e10de50b8109b2e1d58309d871044c84ce1
jhosep7 <jodia.ms@outlook.com>
2019-11-12 14:56:39 -0500

    1-create_file.c3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{return (-1); }
-	Input = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	Input = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (Input == -1)
 	{return (-1); }
 	if (text_content == NULL)
