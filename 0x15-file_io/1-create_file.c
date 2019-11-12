3b8c631ebbf676b3ca187159efa58df43a8f3121
jhosep7 <jodia.ms@outlook.com>
2019-11-12 15:16:36 -0500

    1-create_file.c3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,10 @@ int create_file(const char *filename, char *text_content)
 	if (Input == -1)
 	{return (-1); }
 	if (text_content == NULL)
-	{return (1); }
+	{
+		text_content = "";
+		return (1);
+	}
 	for (i = 0; text_content[i] != '\0'; i++)
 	{; }
 	Output =  write(Input, text_content, i);
