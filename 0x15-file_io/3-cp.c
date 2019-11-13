c68b9fffb394a488e41a7c21ace2701555b800ef
jhosep7 <jodia.ms@outlook.com>
2019-11-13 10:56:51 -0500

    3-cp.c2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,4 @@
 #include "holberton.h"
-#include <sys/stat.h>
-#include <fcntl.h>
 /**
  * main - check the code for Holberton School students.
  * @argc:int
@@ -9,50 +7,35 @@
  */
 int main(int argc, char *argv[])
 {
-	int FileA, FileB, Reader, Writer;
+	int FileA, FileB, Reader = 1, Writer;
 	char Buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(2,"Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	FileA = open(argv[1], O_RDONLY);
 	if (FileA == -1)
 	{
-		dprintf(2,"Error: Can't read from file %s\n",argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	FileB = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
 	if (FileB == -1)
-	{
-		dprintf(2,"Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	while (Reader == 1024)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while (Reader > 0)
 	{
 		Reader = read(FileA, Buffer, 1024);
 		if (Reader == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		Writer = write(FileB, Buffer, Reader);
 		if (Writer == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (close(FileA) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", FileA);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", FileA), exit(100);
 	if (close(FileB) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", FileB);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", FileB), exit(100);
 	return (0);
 }
