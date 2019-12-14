74848fb289795efbced532392ecde80d6528e8b1
jhosep7 <jodia.ms@outlook.com>
2019-12-14 18:32:42 -0500

    7-insert_dnodeint.c5

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,39 +12,38 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 
 	NewNode = malloc(sizeof(dlistint_t));
-	if (NewNode == NULL)
-	{return (NULL);	}
+	if (NewNode == NULL || h == NULL)
+	{return (NewNode ? free(NewNode), NULL : NULL);	}
 	NewNode->n = n;
-	NewNode->next = NULL;
-	if (h == NULL)
-	{return (NULL); }
+//	if (h == NULL)
+//	{return (NULL); }
 	Temp1 = *h;
 	if (!idx)
 	{
-		return (add_dnodeint(h, n));
-/*		NewNode->prev = NULL;
+		NewNode->prev = NULL;
 		NewNode->next = Temp1;
 		if (Temp1)
 		{Temp1->prev = NewNode; }
 		return (*h = NewNode);
-*/	}
+	}
 
 	Temp2 = (*h)->next;
 	for (i = 0; Temp1 && i < (idx - 1); i++)
 	{Temp1 = Temp1->next, Temp2 = Temp2->next; }
 
-	if (!Temp1 && i == idx)
-	{return (add_dnodeint_end(h, n)); }
+//	if (!Temp1 && i == idx)
+//	{return (add_dnodeint_end(h, n)); }
 
-	else if (Temp1)
-	{
-		NewNode->next = Temp2;
-		Temp1->next = NewNode;
-		Temp2->prev = NewNode;
+//	else if (Temp1)
+//	{
 		NewNode->prev = Temp1;
+		NewNode->next = Temp2;
+		if (NewNode->next)
+		{Temp2->prev = NewNode; }
+		Temp1->next = NewNode;
 		return (NewNode);
-	}
-	else
+//	}
+//	else
 //	{Temp1->next = NewNode, Temp1->next = NewNode; }
 	return (free(NewNode), NULL);
 }
