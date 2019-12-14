6adee89d9b2723982ef79ea99caf72a3ba64bba3
jhosep7 <jodia.ms@outlook.com>
2019-12-14 18:19:24 -0500

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,18 +21,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	Temp1 = *h;
 	if (!idx)
 	{
-		NewNode->prev = NULL;
-		NewNode->next = Temp1 ? Temp1 : NULL;
+		return (add_dnodeint(h, n));
+/*		NewNode->prev = NULL;
+		NewNode->next = Temp1;
 		if (Temp1)
 		{Temp1->prev = NewNode; }
 		return (*h = NewNode);
-	}
+*/	}
+
 	Temp2 = (*h)->next;
 	for (i = 0; Temp1 && i < (idx - 1); i++)
 	{Temp1 = Temp1->next, Temp2 = Temp2->next; }
-	if (Temp1->next == NULL)
-	{Temp1->next = NULL; }
-	else if (Temp1 != NULL)
+
+	if (!Temp1 && i == idx)
+	{return (add_dnodeint_end(h, n)); }
+
+	else if (Temp1)
 	{
 		NewNode->next = Temp2;
 		Temp1->next = NewNode;
@@ -41,6 +45,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NewNode);
 	}
 	else
-	{Temp1->next = NewNode, Temp1->next = NewNode; }
+//	{Temp1->next = NewNode, Temp1->next = NewNode; }
 	return (free(NewNode), NULL);
 }
