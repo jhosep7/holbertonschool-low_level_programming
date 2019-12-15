dc64b60d08a20bf3c4747d6afeb3f5da1f680e16
jhosep7 <jodia.ms@outlook.com>
2019-12-14 20:59:50 -0500

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!h)
 	{return (NULL); }
 	if (idx == 0)
-	{return (add_dnodeint_end(h, n)); }
+	{return (add_dnodeint(h, n)); }
 
 	NewNode = malloc(sizeof(dlistint_t));
 	if (!NewNode)
