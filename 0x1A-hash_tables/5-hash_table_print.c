b5b3285fc52956e18f21a6376b00dec230751b3e
jhosep7 <jodia.ms@outlook.com>
2020-01-18 23:23:41 -0500

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,18 +9,19 @@ void hash_table_print(const hash_table_t *ht)
 {
 unsigned long int i, j = 0;
 hash_node_t *Temp;
+
 if (!ht)
 {return; }
 printf("{");
-for (i = j; i < ht->size; i++)
-{
-if (ht->array[i])
+for (i = 0; i < ht->size; i++)
 {
 Temp = ht->array[i];
 for (; Temp; Temp = Temp->next)
 {
-printf("%s'%s':'%s'", j == 0 ? "" : ", ", Temp->key, Temp->value)
-}
+if (j)
+{printf(", "); }
+printf("'%s':'%s'", Temp->key, Temp->value);
+j++;
 }
 }
 printf("}\n");
